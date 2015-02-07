92a20fcd2ca9f0cedf2c837d65a9a75502ab3140
Roope Salmi <rpsalmi@gmail.com>
2015-02-07 13:35:20 +0200

    Add a comment to db.php

--- httpdocs/db.php
+++ httpdocs/db.php
@@ -1,5 +1,5 @@
 <?php
-	
+	//The database password is stored in a file that git will not track for security reasons.
 	$yht = mysqli_connect("localhost","kavija",trim(file_get_contents(dirname(__FILE__) . "/dbpassword.nogit")),"sivusto");
 	mysqli_set_charset($yht, "utf8");
 	if (mysqli_connect_errno()) {
