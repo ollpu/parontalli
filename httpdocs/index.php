17ec56dafac1c6116e733c333a476c84bc34436b
Roope Salmi <rpsalmi@gmail.com>
2015-02-07 13:43:01 +0200

    Add reference to this repository on index

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -109,7 +109,7 @@ echo $html;
 <br><br><br><br><br><br><br><br>
 <div class="footer">
 <hr>
-<a href="./ohjaus">Hallinta</a> - Sivuston rakenteen tehnyt: <a href="mailto:rpsalmi@gmail.com">Roope Salmi</a>
+<a href="./ohjaus">Hallinta</a> - Sivuston rakenteen tehnyt: <a href="mailto:rpsalmi@gmail.com">Roope Salmi</a> - <a href="https://github.com/ollpu/parontalli">Lähdekoodi</a>
 </div>
 </body>
 <html>
