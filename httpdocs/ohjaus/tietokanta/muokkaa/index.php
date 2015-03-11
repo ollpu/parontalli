2e862a32e2d0dfd1b491d4f2e2da7c82eb5ae6fc
Roope Salmi <rpsalmi@gmail.com>
2015-03-11 21:12:51 +0200

    Actually add the progress of editing interface
    Forgot to add the file itself, whoops :smile:

---
+++ httpdocs/ohjaus/tietokanta/muokkaa/index.php
@@ -0,0 +1,106 @@
+<!--Kotisivut Riikka/Hevoset; /ohjaus/tietokanta/muokkaa/index.php-->
+
+<?
+$rel = "../../../";
+
+include $rel."db.php";
+
+include $rel."ohjaus/passphrase.php";
+
+
+
+?>
+<html>
+<head>
+
+<?php include $rel."skeleton/metas.php"; ?>
+
+
+<title>
+	Tietokanta - Muokkaa
+</title>
+
+<?php include $rel."skeleton/styles.php"; ?>
+
+</head>
+
+
+<body>
+
+<?php include $rel."skeleton/header.php"; ?>
+
+<div class="nav">
+	<?php
+		createLink("sininen", "../", 	"", 				"Takaisin"			               );
+		createLink("vihrea", 	"./", 	"thispage", "Muokkaa eläintä tietokannassa");
+	?>
+
+	<hr class="header">
+</div>
+
+
+
+
+<div class="content"><br>
+
+<br/>
+
+
+<?php
+
+
+if($logged) {
+	$animalid = $_GET['id'];
+  
+  $query = mysqli_query($yht, "SELECT * FROM `animals` WHERE id_name = '".$animalid."' LIMIT 1");
+	$row = mysqli_fetch_assoc($query);
+	
+  echo("
+  <form name='edit' method='POST' action='./tallenna.php'>
+  	<table border='0'>
+  		<tr>
+  			<td>Nimi: </td>
+        <td><input type='text' name='id_name' value='$animalid' disabled/></td>
+        <td>&nbsp&nbspEläimen \"kutsumanimi\" (ei koskaan näytetä kävijälle).</td>
+  		</tr>
+      <tr>
+        <td>Otsikko: </td>
+        <td><input type='text' name='name' value='". htmlspecialchars($row['name']) ."'/></td>
+      </tr>
+      <tr>
+        <td>Kuvat: </td>
+        <td><textarea name='img' rows='3' cols='20'>". htmlspecialchars($row['img']) ."</textarea></td>
+        <td>&nbsp&nbspKuvia eläimestä (linkkeinä). Erota linkit ',' merkeillä ja rivit ';' merkeillä. Yhdellä rivillä voi olla enintään 4 kuvaa.</td>
+      </tr>
+      <tr>
+        <td>Linkit:</td>
+        <td><textarea name='link' rows='2' cols='20'>". htmlspecialchars($row['link']) ."</textarea></td>
+      </tr>
+      <tr>
+        <td>Sukuposti-linkki: </td>
+        <td><input type='text' name='sukuposti' value='". htmlspecialchars($row['sukuposti']) ."'/></td>
+        <td>&nbsp&nbspJätä tyhjäksi jos ei ole.</td>
+      </tr>
+      <tr>
+        <td>Leipäteksti: </td>
+        <td><textarea name='text' rows='10' cols='30'>". htmlspecialchars($row['text']) ."</textarea></td>
+      </tr>
+      <tr>
+        <td>Hinta: </td>
+        <td><input type='text' name='price' value='". htmlspecialchars($row['price']) ."'/></td>
+        <td>&nbsp&nbspVoit valita sijoituskohtaisesti, näytetäänkö hintaa vai ei.</td>
+      </tr>
+  	</table>
+  </form>
+	");
+	
+} else echo("Et ole kirjautunut sisään! Yritä uudelleen <a href='../../'>tästä</a>.");
+?>
+
+
+</div>
+
+<?php include $rel."skeleton/footer.php" ?>
+
+</body>
+<html>
