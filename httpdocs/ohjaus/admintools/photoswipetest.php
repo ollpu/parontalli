11aa8ca1ef755453898cfbf16bafc24e535595a5
Roope Salmi <rpsalmi@gmail.com>
2015-06-13 01:46:22 +0300

    Initial PhotoSwipe implementation
     - Include photoswipe dist/ folder
     - Test it on /ohjaus/admintools/photoswipetest.php
     - (Auxiliary JS and structure HTML contained in skeleton/photoswipe.php)

---
+++ httpdocs/ohjaus/admintools/photoswipetest.php
@@ -0,0 +1,98 @@
+
+
+<!--Kotisivut Riikka/Hevoset; /template/graphic.php-->
+<?
+
+//Specify relation to / (or /httpdocs/)
+$rel = "../../";
+
+include $rel."db.php";
+
+?>
+<html>
+<head>
+
+<?php include $rel."skeleton/metas.php" ?>
+
+
+<title>
+	Template
+</title>
+
+<?php include $rel."skeleton/styles.php" ?>
+
+</head>
+
+
+<body>
+
+<?php include $rel."skeleton/header.php" ?>
+
+<div class="nav">
+	<?php
+		createLink("sininen", "./", "thispage", "Template Site");
+	?>
+
+	<hr class="header">
+</div>
+
+<!--img src="image_source" class="header"-->
+
+<? include $rel.'skeleton/photoswipe.php' ?>
+
+<div class="content"><br>
+  
+  <?php
+    function ranger($url){
+      $headers = array(
+      "Range: bytes=0-32768"
+      );
+
+      $curl = curl_init($url);
+      curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+      $data = curl_exec($curl);
+      curl_close($curl);
+      return $data;
+    }
+    function getDimensions($url) {
+      $raw = ranger($url);
+      $im = imagecreatefromstring($raw);
+
+      $width = imagesx($im);
+      $height = imagesy($im);
+
+      return "{$width}x{$height}";
+    }
+    
+  ?>
+  
+  <div class="my-gallery" itemscope itemtype="http://schema.org/ImageGallery">
+
+    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+      <a href="http://i.imgur.com/Uk36p9o.jpg" itemprop="contentUrl" data-size="<? echo(getDimensions('http://i.imgur.com/Uk36p9o.jpg')); ?>">
+        <img src="http://i.imgur.com/Uk36p9om.jpg" itemprop="thumbnail" alt="Image description" />
+      </a>
+      <figcaption itemprop="caption description">Kuva 1.</figcaption>
+    </figure>
+
+    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+      <a href="http://i.imgur.com/a3ESsFP.jpg" itemprop="contentUrl" data-size="<? echo(getDimensions('http://i.imgur.com/a3ESsFP.jpg')); ?>">
+        <img src="http://i.imgur.com/a3ESsFPm.jpg" itemprop="thumbnail" alt="Image description" />
+      </a>
+      <figcaption itemprop="caption description">Kuva 2.</figcaption>
+    </figure>
+
+
+  </div>
+
+  
+  
+  
+
+</div>
+
+<?php include $rel."skeleton/footer.php" ?>
+
+</body>
+<html>
--- httpdocs/skeleton/styles.php
+++ httpdocs/skeleton/styles.php
@@ -1,3 +1,20 @@
 <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" class="text/css" href="<? print($rel); ?>main.css">
+
+<!-- PhotoSwipe -->
+  <? $psPath = $rel.'photoswipe/' ?>
+  <!-- Core CSS file -->
+  <link rel="stylesheet" href="<? echo($psPath); ?>photoswipe.css">
+
+  <!-- Skin CSS file (styling of UI - buttons, caption, etc.)
+       In the folder of skin CSS file there are also:
+       - .png and .svg icons sprite,
+       - preloader.gif (for browsers that do not support CSS animations) -->
+  <link rel="stylesheet" href="<? echo($psPath); ?>default-skin/default-skin.css">
+
+  <!-- Core JS file -->
+  <script src="<? echo($psPath); ?>photoswipe.min.js"></script>
+
+  <!-- UI JS file -->
+  <script src="<? echo($psPath); ?>photoswipe-ui-default.min.js"></script>
